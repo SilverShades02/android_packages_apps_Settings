f2549dc62d51e77f9bb7fe11c2e15c451be9c690
xyyx <xyyx@mail.ru>
2018-02-19 10:30:16 +0800

    Changelog: Add left padding
    before: https://imgur.com/a/TzTAD
    after: https://imgur.com/a/EPjqT
    
    Change-Id: I14e4c6161e4096fc77b408254aa05ff4aae7b05b

--- src/com/android/settings/lluvia/Changelog.java
+++ src/com/android/settings/lluvia/Changelog.java
@@ -29,6 +29,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
@@ -90,6 +92,9 @@ public class Changelog extends Fragment {
           sb.setSpan(new StyleSpan(Typeface.BOLD+Typeface.ITALIC),m.start(1), m.end(1), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
         }
         final TextView textView = new TextView(getActivity());
+        LinearLayout.LayoutParams llp = new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        llp.setMargins(20, 0, 0, 0); // llp.setMargins(left, top, right, bottom);
+        textView.setLayoutParams(llp);
         textView.setText(sb);
 
         final ScrollView scrollView = new ScrollView(getActivity());
